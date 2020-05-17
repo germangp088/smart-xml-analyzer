d918e532f97084bc2a49c8b16bbd66ee8ff5c9b6
germangp088 <germangp088@gmail.com>
2020-05-17 12:53:49 -0300

    Add diff find

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 const Crawler = require("crawler");
 
 const selector = process.env.SELECTOR || "#make-everything-ok-button";
- 
-const c = new Crawler({
-    maxConnections : 10,
-    callback : (error, res, done) => {
-        if(error){
-            console.log(error);
-        }else{
-            const $ = res.$;
-            let xPathTags = [];
-            if($(selector)['0']) {
-                xPathTags.push(`${$(selector)['0'].name} > `);
-                let parent = $(selector)['0'].parent
-                while (parent) {
-                    xPathTags.push(`${parent.name} > `);
-                    parent = parent.parent;
-                }
-                let xPath = xPathTags.reverse().join("").trim();
-                xPath = xPath.substring(0, xPath.length - 1).trim();
-                console.log(xPath);
+
+let stop = false;
+
+const execCrawler = (findOrigin, originXPath) => {
+
+    const crawler = new Crawler({
+        maxConnections : 10,
+        callback : (error, res, done) => {
+            if(error) {
+                console.log(error);
             } else {
-                console.log("Tag was not found");
+                let xPath
+                const $ = res.$;
+                let xPathTags = [];
+                const element = originXPath ? $(originXPath)['0'] : $(selector)['0']
+                if(element) {
+                    xPathTags.push(`${element.name} > `);
+                    let parent = element.parent
+                    while (parent) {
+                        xPathTags.push(`${parent.name} > `);
+                        parent = parent.parent;
+                    }
+                    xPath = xPathTags.reverse().join("").trim();
+                    xPath = xPath.substring(0, xPath.length - 1).trim();
+                    console.log(xPath);
+                } else {
+                    console.log("Tag was not found");
+                }
+                if(!stop) {
+                    console.log({element})
+                    stop = true;
+                    // Call function recursivelly to reuse code.
+                    execCrawler(false, xPath)
+                }
             }
+            done();
         }
-        done();
+    });
+
+    if(findOrigin){
+        // Queue input origin file, if filename is not provided it will take sample-0-origin as default.
+        crawler.queue(`http://localhost:8080/${process.env.INPUT_ORIGIN_FILE || 'sample-0-origin'}.html`);
+    } else {
+        crawler.queue(`http://localhost:8080/${process.env.INPUT_OTHER_FILE}.html`);
     }
-});
+}
 
-// Queue input origin file, if filename is not provided it will take sample-0-origin as default.
-c.queue(`http://localhost:8080/${process.env.INPUT_ORIGIN_FILE || 'sample-0-origin'}.html`);
-
-c.queue(`http://localhost:8080/${process.env.INPUT_OTHER_FILE}.html`);
+execCrawler(true);
