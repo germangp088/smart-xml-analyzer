8fdb08db78fb72bfe02fc867cb26e0920d1ff8cf
germangp088 <germangp088@gmail.com>
2020-05-17 12:34:06 -0300

    Add xPath builder

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-
 const Crawler = require("crawler");
 
 const selector = process.env.SELECTOR || "#make-everything-ok-button";
  
 const c = new Crawler({
     maxConnections : 10,
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
+    callback : (error, res, done) => {
         if(error){
             console.log(error);
         }else{
             const $ = res.$;
-            console.log($(selector).text());
+            let xPathTags = [];
+            if($(selector)['0']) {
+                xPathTags.push(`${$(selector)['0'].name} > `);
+                let parent = $(selector)['0'].parent
+                while (parent) {
+                    xPathTags.push(`${parent.name} > `);
+                    parent = parent.parent;
+                }
+                let xPath = xPathTags.reverse().join("").trim();
+                xPath = xPath.substring(0, xPath.length - 1).trim();
+                console.log(xPath);
+            } else {
+                console.log("Tag was not found");
+            }
         }
         done();
     }
 });
 
-// Queue just one URL, with default callback
+// Queue input origin file, if filename is not provided it will take sample-0-origin as default.
 c.queue(`http://localhost:8080/${process.env.INPUT_ORIGIN_FILE || 'sample-0-origin'}.html`);
+
+c.queue(`http://localhost:8080/${process.env.INPUT_OTHER_FILE}.html`);
